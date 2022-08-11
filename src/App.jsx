5a2ee129117820398bf08440ee02b9c7f47df207
Luis Caraballo <lcaraballom@pm.me>
2022-08-11 15:53:48 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,138 @@
+function App () {
+
+  return (
+    <div className="w-full px-6 pb-12 antiliased bg-white">
+      <div className="mx-auto max-w-8xl">
+        <div className="dark:bg-gray-900 mt-6">
+          <div className="max-w-screen-lg md:flex mx-auto dark:bg-gray-900">
+            <div className="md:w-1/3 p-2 md:flex md:justify-center">
+              <div className="md:fixed">
+                <div className="md:block flex justify-center items-center">
+                  <div className="mb-2 mx-7 mt-4 justify-center items-center">
+                    <h1 className="md:text-3xl text-2xl text-gray-800 font-bold dark:text-blue-100">
+                      Md Solaiman
+                    </h1>
+                    <div className="md:text-lg text-gray-600 dark:text-blue-100">
+                      Full Stack Developer
+                    </div>
+
+                    <a href="mailto:hello@rathik.dev" className="text-gray-600 md:hidden mt-1 dark:text-blue-100">
+                      hello@rathik.dev
+                    </a>
+                  </div>
+                </div>
+                <div className="mx-7 hidden md:block">
+                  <div className="my-2 text-gray-600 flex dark:text-blue-100">
+                    <i className="bi bi-envelope-fill"></i>
+                    <a href="mailto:hello@rathik.dev"> hello@rathik.dev</a>
+                  </div>
+                  <div className="my-2 text-gray-600 flex dark:text-blue-100">
+                    <i className="bi bi-"></i>
+                    <p>Dhaka, Bangladesh</p>
+                  </div>
+                  <div className="my-2 text-gray-600 flex dark:text-blue-200">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 50 50" className="mr-1"><path d="M17.791 46.836A1.999 1.999 0 0019 45v-5.4c0-.197.016-.402.041-.61A.611.611 0 0119 39h-3.6c-1.5 0-2.8-.6-3.4-1.8-.7-1.3-1-3.5-2.8-4.7-.3-.2-.1-.5.5-.5.6.1 1.9.9 2.7 2 .9 1.1 1.8 2 3.4 2 2.487 0 3.82-.125 4.622-.555C21.356 34.056 22.649 33 24 33v-.025c-5.668-.182-9.289-2.066-10.975-4.975-3.665.042-6.856.405-8.677.707a21.537 21.537 0 01-.151-.987c1.797-.296 4.843-.647 8.345-.714a8.162 8.162 0 01-.291-.849c-3.511-.178-6.541-.039-8.187.097-.02-.332-.047-.663-.051-.999 1.649-.135 4.597-.27 8.018-.111a9.832 9.832 0 01-.13-1.543c0-1.7.6-3.5 1.7-5-.5-1.7-1.2-5.3.2-6.6 2.7 0 4.6 1.3 5.5 2.1C21 13.4 22.9 13 25 13s4 .4 5.6 1.1c.9-.8 2.8-2.1 5.5-2.1 1.5 1.4.7 5 .2 6.6 1.1 1.5 1.7 3.2 1.6 5 0 .484-.045.951-.11 1.409 3.499-.172 6.527-.034 8.204.102-.002.337-.033.666-.051.999-1.671-.138-4.775-.28-8.359-.089a8.272 8.272 0 01-.325.98c3.546.046 6.665.389 8.548.689-.043.332-.093.661-.151.987-1.912-.306-5.171-.664-8.879-.682-1.665 2.878-5.22 4.755-10.777 4.974V33c2.6 0 5 3.9 5 6.6V45c0 .823.498 1.53 1.209 1.836C41.37 43.804 48 35.164 48 25 48 12.318 37.683 2 25 2S2 12.318 2 25c0 10.164 6.63 18.804 15.791 21.836z"></path></svg>
+                    <a href="https://github.com/mdrathik"> mdrathik</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="md:w-2/3 px-8">
+              <div className="mb-6">
+                <h2 className="mt-6 text-4xl text-gray-700 font-bold capitalize dark:text-blue-200">
+                  👋 Hola , Good Day !
+                </h2>
+                <p className="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industry's standard dummy text ever since the
+                  1500s, when an unknown printer took a galley of type and scrambled it to
+                  make a type specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining essentially
+                  unchanged. It was popularised in the 1960s with the release of Letraset
+                  sheets containing Lorem Ipsum passages, and more recently with desktop
+                  publishing software like Aldus PageMaker including versions of Lorem
+                  Ipsum.
+                </p>
+                <p className="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industry's standard dummy text ever since the
+                  1500s, when an unknown printer took a galley of type and scrambled it to
+                  make a type specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining essentially
+                  unchanged. It was popularised in the 1960s with the release of Letraset
+                  sheets containing Lorem Ipsum passages, and more recently with desktop
+                  publishing software like Aldus PageMaker including versions of Lorem
+                  Ipsum.
+                </p>
+                <p className="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industry's standard dummy text ever since the
+                  1500s, when an unknown printer took a galley of type and scrambled it to
+                  make a type specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining essentially
+                  unchanged. It was popularised in the 1960s with the release of Letraset
+                  sheets containing Lorem Ipsum passages, and more recently with desktop
+                  publishing software like Aldus PageMaker including versions of Lorem
+                  Ipsum.
+                </p>
+                <p className="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industry's standard dummy text ever since the
+                  1500s, when an unknown printer took a galley of type and scrambled it to
+                  make a type specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining essentially
+                  unchanged. It was popularised in the 1960s with the release of Letraset
+                  sheets containing Lorem Ipsum passages, and more recently with desktop
+                  publishing software like Aldus PageMaker including versions of Lorem
+                  Ipsum.
+                </p>
+                <p className="mt-8 mb-4 dark:bg-gray-800 dark:text-blue-200 text-justify">
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                  Lorem Ipsum has been the industry's standard dummy text ever since the
+                  1500s, when an unknown printer took a galley of type and scrambled it to
+                  make a type specimen book. It has survived not only five centuries, but
+                  also the leap into electronic typesetting, remaining essentially
+                  unchanged. It was popularised in the 1960s with the release of Letraset
+                  sheets containing Lorem Ipsum passages, and more recently with desktop
+                  publishing software like Aldus PageMaker including versions of Lorem
+                  Ipsum.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col items-center mt-16">
+        <div className="flex mb-3 space-x-4">
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/mdrathik" className="text-sm text-gray-500 transition hover:text-gray-600">
+            <span className="sr-only">github</span>
+            <i className="bi bi-github"></i>
+          </a>
+          <a target="_blank" rel="noopener noreferrer" href="#" className="text-sm text-gray-500 transition hover:text-gray-600">
+            <span className="sr-only">Linkedin</span>
+            <i className="bi bi-linkedin"></i>
+          </a>
+          <a target="_blank" rel="noopener noreferrer" href="mailto:hello@rathik.dev" className="text-sm text-gray-500 transition hover:text-gray-600">
+            <span className="sr-only">mail</span>
+            <i className="bi bi-envelope-fill"></i>
+          </a>
+          <a target="_blank" rel="noopener noreferrer" className="text-sm text-gray-500 transition hover:text-gray-600">
+            <span className="sr-only">facebook</span>
+            <i className="bi bi-facebook"></i>
+          </a>
+        </div>
+        <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
+          <div>Copyright © 2022</div>
+          <div>•</div>
+          <a href="/">Luis Caraballo - Let's start coding</a>
+        </div>
+        <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
+          <a target="_blank" rel="noopener noreferrer" href="https://github.com/mdrathik/tailwind-nuxtjs-starter-blog"></a>
+        </div>
+      </div>
+    </div>
+
+  )
+}
+
+export default App
